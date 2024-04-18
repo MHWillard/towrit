8267251ba8f4e11458ff3722881d711e22aa80bb
MHWillard <mhwillard@gmail.com>
2024-04-18 15:55:53 -0400

    daily commit

--- Controllers/RestAPIController.cs
+++ Controllers/RestAPIController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
 using App.Data;
 using App.Models;
 
 namespace App.Controllers
 {
+    [ApiController]
     public class RestAPIController : Controller
     {
         private readonly DBHelper _db;
@@ -12,8 +14,37 @@ namespace App.Controllers
         }
 
         // GET user
+        [HttpGet]
+        [Route("api/[controller]/GetUser")]
+        public IActionResult Get(string username) {
+            UserModel data = _db.GetUser(username);
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return View(data);
+
+        }
+
         // GET user posts
+        [HttpGet]
+        [Route("api/[controller]/GetPosts")]
+        public IActionResult Get(string username) {
+            UserModel user = _db.GetUser(username);
+            //get userPostModel
+            //use that to look up posts
+            //then finally return posts as a list
+        }
+
         // POST new post
+        [HttpPost]
+        [Route("api/[controller]/AddPost")]
+        public IActionResult Post() { }
+
         // POST new user
+        [HttpPost]
+        [Route("api/[controller]/AddUser")]
+        public IActionResult Post() { }
     }
 }
